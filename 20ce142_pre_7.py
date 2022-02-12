672c8e96ea199a44a7d27dee36204ddeeda2cd47
KHUSHI1102 <96758617+KHUSHI1102@users.noreply.github.com>
2022-02-12 19:24:07 +0530

    Update 20ce142_pre_7.py

--- 20ce142_pre_7.py
+++ 20ce142_pre_7.py
@@ -1,12 +1,15 @@
-T = int(input())
-for i in range(T):
-    n = input()
-    l = len(n)
-    if l % 2 == 0:
-        b, c = n[:l//2], n[l//2:]
+'''NAME : khushi tala
+   ID   : 20Ce142
+'''
+T = int(input())                     #take number of test cases from user
+for i in range(T):  
+    n = input()                      #take string from user
+    l = len(n)                       #length of n
+    if l % 2 == 0:                   #check whether length divisible by two or not
+        b, c = n[:l//2], n[l//2:]    #slicing
     else:
-        b, c = n[:l//2], n[l//2+1:]
-    if sorted(b) == sorted(c):
+        b, c = n[:l//2], n[l//2+1:]  #slicing
+    if sorted(b) == sorted(c):       #compare two sorted string
         print("YES")
     else:
         print("NO")
